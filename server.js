e2b278d2f67669872c4635612bf9653fb9190393
Gustavo <diazgus411@gmail.com>
2023-06-28 21:15:42 -0700

    completed file structure, added userjs code, added and changed files in views and updated serverjs file

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+// const session = require('express-session');
 const exphbs = require('express-handlebars');
+// const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const app = express();
