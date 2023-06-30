da8ad14d866a228dbc867cc81d0cd2238f25ee0d
Gustavo <diazgus411@gmail.com>
2023-06-29 20:29:31 -0700

    added partially working register page, missing seeds data. Also added models, routes and dotenv

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,6 +28,32 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/register', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    // const projectData = await Project.findAll({
+    //   include: [
+    //     {
+    //       model: User,
+    //       attributes: ['name'],
+    //     },
+    //   ],
+    // });
+
+    // // Serialize data so the template can read it
+    // const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // // Pass serialized data and session flag into template
+    // res.render('homepage', { 
+    //   projects, 
+    //   logged_in: req.session.logged_in 
+    // });
+    res.render('registration');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // router.get('/project/:id', async (req, res) => {
 //   try {
 //     const projectData = await Project.findByPk(req.params.id, {
--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,2 @@
-DROP TABLE IF EXISTS project_db;
+DROP DATABASE IF EXISTS project_db;
 CREATE DATABASE project_db;
-
-USE project_db;
-
--- Create the users table
-CREATE TABLE users (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(30) NOT NULL,
-  password VARCHAR(30) NOT NULL
-);
